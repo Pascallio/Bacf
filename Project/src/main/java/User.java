78ab71eefada06fb83ee6f2178c27e0d7dc4e597
Pascal Maas <p.maas92@gmail.com>
2017-01-03 14:29:33 +0100

    User class

---
+++ Project/src/main/java/User.java
@@ -0,0 +1,57 @@
+/**
+ * Created by pascal on 3-1-17.
+ */
+public class User {
+
+    private String name;
+    private int numOfLifes;
+    private int numOfBombs;
+    private String avatar;
+    private int time;
+
+    public User(String name, String avatar){
+        this.name = name;
+        this.avatar = avatar;
+    }
+
+    public User(String name, String avatar, int numOfLifes, int numOfBombs){
+        this.name = name;
+        this.avatar = avatar;
+        this.numOfLifes = numOfLifes;
+        this.numOfBombs = numOfBombs;
+
+    }
+
+    public int getNumOfBombs(){
+        return this.numOfBombs;
+    }
+
+    public int getNumOfLifes(){
+        return this.numOfLifes;
+    }
+
+    public boolean hasLifesLeft(){
+        return this.numOfLifes > 0;
+    }
+
+    public boolean hasBombsLeft(){
+        return this.numOfBombs > 0;
+    }
+
+    public void setTime(int seconds){
+        this.time = seconds;
+    }
+
+    public int getTime(){
+        return this.time;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getAvatar(){
+        return this.avatar;
+    }
+
+}
